80edb44f7976639b04fc967ccca7e78a5d5581da
mahambk <mahammbk@gmail.com>
2018-03-20 19:27:18 +0300

    merged createtree template into new_tree

--- familytree/app/forms.py
+++ familytree/app/forms.py
@@ -7,3 +7,9 @@ class TreeForm(forms.ModelForm):
 	class Meta:
 		model = Tree
 		fields = ('name',)
+		#widgets = {
+		#	'name': forms.TextInput(attrs={'class': 'type css class here'}),
+		#}
+		labels = {
+			'name': 'اسم العائلة:',
+		}
